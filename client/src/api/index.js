489cab4df37a20b796634a005c6f01c7fad481f0
Vansh Mago <vanshmago9@gmail.com>
2023-01-13 02:07:16 +0530

    changing server hosting from heroku to cyclic

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://lingolands.herokuapp.com" });
+const API = axios.create({ baseURL: "https://lingolandserver.cyclic.app" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
